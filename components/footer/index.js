ea3ca9c3e876e753d440a913eb2119df5fea69b3
Makstosei <76548166+Makstosei@users.noreply.github.com>
2023-07-31 12:46:01 +0300

    main
    trial

--- components/footer/index.js
+++ components/footer/index.js
@@ -14,7 +14,7 @@ function Footer() {
       </div>
       <div>
         &nbsp; as part of&nbsp;
-        <Link className={styles.colorBlue} href="https://twitter.com/_mehmetpekcan" target="_blank">
+        <Link className={styles.colorBlue} href="https://www.youtube.com/playlist?list=PLf3cxVeAm439RsaHrGACExl3o060pM7W2" target="_blank">
           Mehmet Pekcan
         </Link>
         's youtube education
